a42b812b7ae03a390d99dc3bd1c88bfd5e19747c
FredySaputra <153965495+FredySaputra@users.noreply.github.com>
2024-03-16 10:53:10 +0700

    Update jadwal.sql

--- jadwal.sql
+++ jadwal.sql
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `jadwal` (
   `id_jadwal` int(3) NOT NULL,
   `jam_masuk` time DEFAULT NULL,
-  `jam_keluar` time DEFAULT NULL
+  `jam_pulang` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
